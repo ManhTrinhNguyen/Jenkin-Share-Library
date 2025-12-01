5c880ad843cd1183dc779a8fb08ba3eb7863b974
Tim <nguyenmanhtrinh17041998@gmail.com>
2025-12-01 11:58:39 -0800

    Create Complete Increment version

--- vars/incrementNodejsImageVersion.groovy
+++ vars/incrementNodejsImageVersion.groovy
@@ -1,4 +1,10 @@
 #!/user/bin/env groovy
 def call () {
   echo 'Increment nodejs Image Version'
+  dir('app') {
+    sh 'npm install'
+    sh 'npm version patch --no-git-tag-version'
+    def jsonConfig = readJSON file: 'package.json'
+    def VERSION = jsonConfig.version
+    env.IMAGE_VERSION = VERSION 
 }
