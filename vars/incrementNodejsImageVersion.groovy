93a68f4708c4bec5eeacd60f0c5ee4304dd4cee4
Tim <nguyenmanhtrinh17041998@gmail.com>
2025-12-01 12:04:08 -0800

    Edit Increment file

--- vars/incrementNodejsImageVersion.groovy
+++ vars/incrementNodejsImageVersion.groovy
@@ -1,7 +1,7 @@
 #!/user/bin/env groovy
 def call () {
-  echo 'Increment nodejs Image Version'
   dir('app') {
+    echo 'Increment nodejs Image Version'
     sh 'npm install'
     sh 'npm version patch --no-git-tag-version'
     def jsonConfig = readJSON file: 'package.json'
